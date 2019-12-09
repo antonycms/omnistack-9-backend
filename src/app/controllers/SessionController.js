b4128ded5a870c27209be0a99e0948c6ede5350c
Antony Carlos <antonyc828@gmail.com>
2019-12-08 21:50:52 -0300

    added password hash compare

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,16 +1,18 @@
 const User = require('../models/User');
+const { compare } = require('bcrypt');
 
 class SessionController {
   async store(req, res) {
-    const { email } = req.body;
-
-    if (!email) {
-      return res.status(400).json('email invalid');
-    }
+    const { email, password } = req.body;
 
     const user = await User.findOne({ email });
     if (!user) {
       return res.statu(400).json('user not found');
+    }
+
+    const checkPassword = await compare(password, user.password);
+    if (!checkPassword) {
+      return res.status(401).json({ error: 'password does not match' });
     }
 
     return res.json(user);
